58904408931d01eb491f7a06a34d9a88ce95a99a
agomezmartin <101741206+agomezmartin@users.noreply.github.com>
2023-05-08 15:46:54 +0200

    Lunes 8 de mayo

--- desarrollo/20230508/ej9_cine/php/datos_usuario.php
+++ desarrollo/20230508/ej9_cine/php/datos_usuario.php
@@ -7,8 +7,12 @@ if(isset($_POST['enviando'])){
 	$edad = $_POST['edad'];
 
 	// lógica de negocio
-	if($dia == 'lunes' || $dia == 'miércoles' || $dia == 'viernes' && $edad <= 60 ){
-		echo('Como tienes' . $edad . ' años, y vas a venir el '. $dia);
+	if(($dia == 'lunes' || $dia == 'miércoles' || $dia == 'viernes') && $edad >= 60 ){
+		echo('Como tienes ' . $edad . ' años, y vas a venir el '. $dia . ' tienes la entrada gratis');
+	} else if (($dia == 'martes' || $dia == 'jueves') && $edad <= 18){
+		echo('Como tienes ' . $edad . ' años, y vas a venir el '. $dia . ' tienes la entrada gratis');
+	} else {
+		echo('Como tienes ' . $edad . ' años, y vas a venir el '. $dia . ' la entrada te cuesta 10€');
 	}
 
 }
