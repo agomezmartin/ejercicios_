72ec19ad67239195e769550bb011f3fb23cc716b
agomezmartin <101741206+agomezmartin@users.noreply.github.com>
2023-05-08 10:13:09 +0200

    Lunes 8 de mayo

---
+++ desarrollo/20230508/ej8/php/datos_usuario.php
@@ -0,0 +1,24 @@
+<?php
+
+if($_POST['entrando']){
+
+	// captura de datos
+	$u = $_POST['usu'];
+	$c = $_POST['contra'];
+
+	// simulación de datos ya existentes
+	$ur = 'CARLOS';
+	$cr = '12345';
+
+	// lógica de negocio
+	if($u == $ur && $c == $cr){
+		echo('Datos correctos');
+	} else {
+		echo('Datos incorrectos');
+	}
+
+
+}
+
+
+?>
