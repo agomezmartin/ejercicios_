b6a134b14d2b309f6c0315c6c638216a30ecf1cb
Alberto <asdf@gmail.com>
2023-05-17 14:04:58 +0200

    Miércoles 17 de mayo

--- Tarea4_FormularioConEvento/src/app/componentes/formulario/formulario.component.ts
+++ Tarea4_FormularioConEvento/src/app/componentes/formulario/formulario.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class FormularioComponent {
 
+  precio:number = 0;
+  visible="none";
+
+  calcularPrecios(bolso:string, talla:string){
+
+/*    switch (true) {
+      case bolso='Dalia' || bolso='azalea' || bolso='margarita':
+        if(talla=="pequeño"){
+          this.precio=10;
+        }
+        break;
+    
+      default:
+        break;
+    }
+    
+    this.visible="visible";
+    return this.precio, this.visible;
+*/
+  }
+
 }
